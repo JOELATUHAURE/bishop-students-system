ab13f2fae57844276e10d74bd366bcba91d975e0
JOELATUHAURE <joelatuhaire20@gmail.com>
2025-05-06 10:19:41 +0300

    vite update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'client/dist', // Ensure this matches your Netlify publish directory
+    outDir: 'dist', // Ensure this matches your Netlify publish directory
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
